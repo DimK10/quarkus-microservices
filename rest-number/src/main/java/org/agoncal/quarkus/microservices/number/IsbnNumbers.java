2ede138d74617b1e06d9a5c539132102b9f77bfa
dkaitantzidis <dkaitantzidis@byte.gr>
2023-10-31 16:13:27 +0200

    Lesson 18 finished

--- rest-number/src/main/java/org/agoncal/quarkus/microservices/number/IsbnNumbers.java
+++ rest-number/src/main/java/org/agoncal/quarkus/microservices/number/IsbnNumbers.java
@@ -1,5 +1,7 @@
 package org.agoncal.quarkus.microservices.number;
 
+import jakarta.json.bind.annotation.JsonbProperty;
+import jakarta.json.bind.annotation.JsonbTransient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +13,13 @@ import java.time.Instant;
 @Builder
 public class IsbnNumbers {
 
+    @JsonbProperty("isbn_13")
     private String isbn13;
 
+    @JsonbProperty("isbn_10")
     private String isbn10;
 
+    @JsonbTransient
     private Instant generationTime;
 
 }

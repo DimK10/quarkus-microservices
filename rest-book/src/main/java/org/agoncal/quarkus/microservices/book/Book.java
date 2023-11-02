1e3a913c77ac94ab6977e67dab8962237ae96fd3
dkaitantzidis <dkaitantzidis@byte.gr>
2023-11-02 12:07:35 +0200

    Lesson 32 finished

--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/Book.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/Book.java
@@ -4,15 +4,19 @@ import jakarta.json.bind.annotation.JsonbDateFormat;
 import jakarta.json.bind.annotation.JsonbProperty;
 import lombok.Builder;
 import lombok.Data;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.time.Instant;
 
 @Data
 @Builder
+@Schema(description = "This is a book")
 public class Book {
 
     @JsonbProperty("isbn_13")
+    @Schema(required = true)
     private String isbn13;
+    @Schema(required = true)
     private String title;
     private String author;
     @JsonbProperty("year_of_publication")
@@ -20,5 +24,6 @@ public class Book {
     private String genre;
     @JsonbDateFormat("yyyy-MM-dd")
     @JsonbProperty("creation_date")
+    @Schema(implementation = String.class, format = "date")
     private Instant creationDate;
 }
--- rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,15 +1,16 @@
 package org.agoncal.quarkus.microservices.book;
 
-import io.netty.handler.codec.http.HttpStatusClass;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 
 import java.time.Instant;
 
 @Path("/api/books")
+@Tag(name = "Book REST endpoint")
 public class BookResource {
 
     @Inject
